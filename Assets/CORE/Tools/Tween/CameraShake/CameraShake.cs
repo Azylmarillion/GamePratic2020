78be7c8a0b86c4cdb211b44ef5d38cb3c2dc1c5a
JulienDelaunay <julien.delaunay034@gmail.com>
2020-10-10 11:45:39 +0200

    Imported DOTween, Created movements of the piston minigame

---
+++ Assets/CORE/Tools/Tween/CameraShake/CameraShake.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace GamePratic2020.Tools {
+	[CreateAssetMenu(fileName = "NewCameraShake", menuName = "Utils/Tweening/Camera Shake", order = 0)]
+	public class CameraShake : ScriptableObject {
+        #region Settings
+        [SerializeField, Min(0f)] private float duration = 1f;
+        [SerializeField, Min(0f)] private float strength = 1f;
+        [SerializeField, Min(0f)] private int vibrato = 10;
+        [SerializeField, Min(0f)] private float randomness = 90f;
+        [SerializeField] private bool snapping = false;
+        [SerializeField] private bool fadeOut = true;
+        #endregion
+
+        #region Current
+        private Tween currentTween = null;
+        #endregion
+
+        #region Behaviour
+        /// <summary>
+        /// Play camera shake effect
+        /// </summary>
+        public void Play() {
+            //Must finish the previous tween to play another to not offset the camera
+            if(currentTween != null && currentTween.IsPlaying()) {
+                currentTween.Complete();
+            }
+
+            Camera.main.transform.DOShakePosition(duration, strength, vibrato, randomness, snapping, fadeOut);
+        }
+        #endregion
+    }
+}
---
+++ Assets/CORE/_Gameplay/1nd MG - Crushing/Prefabs/Piston.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using EnhancedEditor;
+using GamePratic2020.Tools;
+
+namespace GamePratic2020 {
+    public class Piston : MonoBehaviour {
+        #region Settings
+        [Section("Settings")]
+        [SerializeField] private float minHeight = 2f;
+        [SerializeField] private float maxHeight = 10f;
+        [SerializeField] private int steps = 5;
+        [SerializeField] private AnimationCurve heightOverSteps = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
+        [Header("References")]
+        [SerializeField] private Transform pistonHeadTransform = null;
+        [SerializeField] private CameraShake crushCameraShake = null;
+        #endregion
+
+        #region Currents
+        private int currentStep = 0;
+        #endregion
+
+        #region Callbacks
+        private void Start() {
+            MovePiston();    
+        }
+
+        private void Update() {
+            if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+                UpdatePistonPos();
+            }
+        }
+        #endregion
+
+        #region Movement
+        private void UpdatePistonPos() {
+            currentStep++;
+
+            if(currentStep > steps) {
+                currentStep = 0;
+                Crush();
+            }
+
+            MovePiston();
+
+            Debug.Log($"Move to step {currentStep}");
+        }
+
+        private void MovePiston() {
+            float t = (float)currentStep / (float)steps;
+            t = heightOverSteps.Evaluate(t);
+            pistonHeadTransform.position = Vector3.Lerp(transform.position + Vector3.up * minHeight, transform.position + Vector3.up * maxHeight, t);
+        }
+
+        private void Crush() {
+            Debug.Log("Crush");
+            crushCameraShake.Play();
+        }
+        #endregion
+
+        #region Debug
+#if UNITY_EDITOR
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(transform.position + Vector3.up * minHeight, transform.position + Vector3.up * maxHeight);
+            Gizmos.DrawWireSphere(transform.position + Vector3.up * minHeight, 0.3f);
+            Gizmos.DrawWireSphere(transform.position + Vector3.up * maxHeight, 0.3f);
+        }
+#endif
+        #endregion
+    }
+}

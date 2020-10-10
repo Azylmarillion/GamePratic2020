7eab6114e1e0f1876a886bb94e690207d0eada40
Alexis Thiébaut <thiebautalexis01@gmail.com>
2020-10-10 02:58:51 +0200

    Start Heating Mini Game

---
+++ Assets/_CORE/_Mini Games/_Heating/Scripts/Thermometer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thermometer : MonoBehaviour
+{
+    [SerializeField] private float initialRatio = .5f; 
+}

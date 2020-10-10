e2a0c48803427abeb7b3799d9efc4c1605f32853
JulienDelaunay <julien.delaunay034@gmail.com>
2020-10-10 12:51:45 +0200

    Removed URP from the project, Created a framerate debugger

---
+++ Assets/CORE/Tools/Performances/FramerateDebugger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace GamePratic2020.Tools.Performances {
+    public class FramerateDebugger : MonoBehaviour {
+        private void OnGUI() {
+            GUILayout.Label($"{ 1.0f / Time.deltaTime}", new GUIStyle {fontSize = 50});
+        }
+    } 
+}

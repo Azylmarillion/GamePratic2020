07e386af3be6be4f12f118d982b116553c673b9b
JulienDelaunay <julien.delaunay034@gmail.com>
2020-10-10 20:26:44 +0200

    End run screen fill effect WIP

--- Assets/CORE/UI/CompletedRunScreen/Scripts/RunEndScreen.cs
+++ Assets/CORE/UI/CompletedRunScreen/Scripts/RunEndScreen.cs
@@ -4,38 +4,75 @@
 //
 // ============================================================================== //
 
+using DG.Tweening;
+using EnhancedEditor;
 using System.Collections;
 using UnityEngine;
 
 namespace GamePratic2020 {
     public class RunEndScreen : MonoBehaviour {
         #region Settings
-        [Header("Fill")]
-        [SerializeField] private float fillTimePerPoints = 0.5f;
+        [Section("Settings")]
+        [SerializeField] private float pointsPerSeconds = 200;
         [SerializeField] private float fillIntensity = 2f;
-        [SerializeField] private float maxFillHeight = 150f;
+        [SerializeField] private float maxFillHeight = 2;
+        [SerializeField] private float minimumFillHeight = 0;
+        [SerializeField] private float initialFillHeight = 0;
+        [SerializeField] private float cokeFallDuration = 0.5f;
+
+        [Header("References")]
+        [SerializeField] private Transform stackParent = null;
+        [SerializeField] private ParticleSystem fallEffect = null;
+        [SerializeField] private ParticleSystem fillEffect = null;
+
+        //[Header("Text")]
+
         #endregion
 
         #region Current
-        private int currentFilledPoints = 0;
+        [Section("Read Only")]
+        [SerializeField, ReadOnly] private int currentFilledPoints = 0;
+        [SerializeField] private bool hasBeenInitialized = false; 
         #endregion
 
         #region Filling 
-
-        [ContextMenu("Fill Debug")]
-        public void FillDebug() {
-            Fill(1000);
-        }
-
         public void Fill(int points) {
             currentFilledPoints = points;
+
+            if (!hasBeenInitialized) {
+                fillEffect.transform.position = Vector3.up * initialFillHeight;
+                hasBeenInitialized = true;
+            } else {
+                if(fillEffect.transform.position.y > minimumFillHeight) {
+                    stackParent.transform.position += Vector3.up * (minimumFillHeight - (fillEffect.transform.position.y));
+                }
+            }
+
+            StartCoroutine(ProcessFillCoroutine());
         }
 
         private IEnumerator ProcessFillCoroutine() {
+            fallEffect.Play();
+
+            yield return new WaitForSeconds(cokeFallDuration);
+
+            fillEffect.Play();
 
             while(currentFilledPoints > 0) {
+                int pointsDecrement = Mathf.RoundToInt(pointsPerSeconds * Time.deltaTime);
+                currentFilledPoints -= pointsDecrement;
+
+                fillEffect.transform.position += Vector3.up * fillIntensity * Time.deltaTime;
+
+                if(fillEffect.transform.position.y > maxFillHeight) {
+                    stackParent.transform.position += Vector3.up * (maxFillHeight - (fillEffect.transform.position.y));
+                }
+
                 yield return null;
             }
+
+            fallEffect.Stop();
+            fillEffect.Stop();
 
             yield return null;
         }
@@ -44,9 +81,17 @@ namespace GamePratic2020 {
         #region Debug
 #if UNITY_EDITOR
         private void OnDrawGizmosSelected() {
-            Vector3 heightPos = transform.position + Vector3.up * maxFillHeight;
+            Vector3 heightPos = Vector3.up * maxFillHeight;
             Gizmos.color = Color.green;
             Gizmos.DrawLine(heightPos + Vector3.right * 100f, heightPos - Vector3.right * 100f);
+
+            Vector3 initialFillPos = Vector3.up * initialFillHeight;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(initialFillPos + Vector3.right * 100f, initialFillPos - Vector3.right * 100f);
+
+            Vector3 minHeightPos = Vector3.up * minimumFillHeight;
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(minHeightPos + Vector3.right * 100f, minHeightPos - Vector3.right * 100f);
         }
 #endif
         #endregion
